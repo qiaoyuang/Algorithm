db785713891d91314a57129dc26cedbb466bf85c
qiaoyuang <qiaoyuang2012@gmail.com>
2017-11-21 11:31:47 +0800

    2017-11-21

--- src/offer/Questions36.kt
+++ src/offer/Questions36.kt
@@ -3,19 +3,41 @@ package offer
 //å°†ä¸€é¢—äºŒå‰æ’åºæ ‘è½¬æ¢æˆä¸€ä¸ªæ’åºçš„åŒå‘é“¾è¡¨,å¹¶è¿”å›è¾ƒå°ä¸€ç«¯çš„ç«¯ç‚¹
 
 fun main(args: Array<String>) {
+	//¹¹ÔìÒ»¸öËÄ²ãµÄÅÅĞò¶ş²æÊ÷
 	val a = BinaryTreeNode<Int>(10)
+	
 	val b = BinaryTreeNode<Int>(6)
 	val c = BinaryTreeNode<Int>(14)
+	
 	val d = BinaryTreeNode<Int>(4)
 	val e = BinaryTreeNode<Int>(8)
 	val f = BinaryTreeNode<Int>(12)
 	val g = BinaryTreeNode<Int>(16)
+	
+	val h = BinaryTreeNode<Int>(3)
+	val i = BinaryTreeNode<Int>(5)
+	val j = BinaryTreeNode<Int>(7)
+	val k = BinaryTreeNode<Int>(9)
+	val l = BinaryTreeNode<Int>(11)
+	val m = BinaryTreeNode<Int>(13)
+	val n = BinaryTreeNode<Int>(15)
+	val o = BinaryTreeNode<Int>(17)
+	
 	a.mLeft = b
 	a.mRight = c
 	b.mLeft = d
 	b.mRight = e
 	c.mLeft = f
 	c.mRight = g
+	d.mLeft = h
+	d.mRight = i
+	e.mLeft = j
+	e.mRight = k
+	f.mLeft = l
+	f.mRight = m
+	g.mLeft = n
+	g.mRight = o
+	
 	var x = a.convert()
 	while (x.mRight != null) {
 		print("${x.mValue} ")
@@ -32,40 +54,50 @@ fun main(args: Array<String>) {
 
 fun <T> BinaryTreeNode<T>.convert(): BinaryTreeNode<T> {
 	fun BinaryTreeNode<T>.traverse(isBig: Boolean, father: BinaryTreeNode<T>? = null): BinaryTreeNode<T> {
-		when {
-			mLeft == null && mRight == null -> {
-			    father?.let {
-				    if (isBig) {
-					    mLeft = it
-				    } else {
-					    mRight = it
-				    }
-			    }
-				return this
-		    }
-			mLeft == null && mRight != null -> {
-				return mRight!!.traverse(true, this)
+		if (mLeft == null && mRight == null) {
+			if (isBig) {
+				mLeft = father
+			} else {
+				mRight = father
 			}
-			mLeft != null && mRight == null -> {
-				return mLeft!!.traverse(false, this)
-			}
-			else -> {
-				if (isBig) {
-					mRight!!.traverse(true, this)
-					val node = mLeft!!.traverse(false, this)
-					node.mLeft = father
-					return node
-				} else {
-					mLeft!!.traverse(false, this)
-					val node = mRight!!.traverse(true, this)
-					node.mRight = father
-					return node
+			return this
+		}
+		mLeft?.let {
+			mLeft = it.traverse(false, this)
+			mLeft!!.mRight = this
+		}
+		mRight?.let {
+			mRight = it.traverse(true, this)
+			mRight!!.mLeft = this
+		}
+		if (isBig) {
+			if (mLeft != null) {
+				var node = mLeft
+				while (node!!.mLeft != null) {
+					node = node.mLeft
 				}
+				return node
 			}
+			return this
+		} else {
+			if (mRight != null) {
+				var node = mRight
+				while (node!!.mRight != null) {
+					node = node.mRight
+				}
+				return node
+			}
+			return this
 		}
 	}
-	mLeft = mLeft?.traverse(false, this)
-	mRight = mRight?.traverse(true, this)
+	mLeft?.let {
+		mLeft = it.traverse(false, this)
+		mLeft!!.mRight = this
+	}
+	mRight?.let {
+		mRight = it.traverse(true, this)
+		mRight!!.mLeft = this
+	}
 	var head = this
 	while (head.mLeft != null) {
 		head = head.mLeft!!
