1d8af49dff5920c3de1729d9417b2c691b7a8ed2
qiaoyuang <qiaoyuang2012@gmail.com>
2024-01-15 16:09:20 +0800

    Finish Questions 88

--- src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions88.kt
+++ src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions88.kt
@@ -3,7 +3,7 @@ package com.qiaoyuang.algorithm.special
 import kotlin.math.min
 
 fun test88() {
-    println(minCost(1, 100, 1, 1, 100, 1))
+    printlnResult(1, 100, 1, 1, 100, 1)
 }
 
 /**
@@ -17,4 +17,22 @@ private fun minCost(vararg costs: Int): Int {
 private fun minCost(costs: IntArray, index: Int): Int = when {
     index < 2 -> costs[index]
     else -> min(minCost(costs, index - 1), minCost(costs, index - 2)) + costs[index]
+}
+
+private fun minCostInLoop(vararg costs: Int): Int {
+    if (costs.size == 2)
+        return costs.last()
+    require(costs.size > 2) { "The size of costs must greater or equal than 2" }
+    val cache = intArrayOf(costs[0], costs[1])
+    for (i in 2 ..< costs.size) {
+        val currentCost = min(cache.first(), cache.last()) + costs[i]
+        cache[0] = cache.last()
+        cache[1] = currentCost
+    }
+    return cache.last()
+}
+
+private fun printlnResult(vararg costs: Int) {
+    val result = minCostInLoop(*costs)
+    println("The minimum cost in ${costs.toList()} is ${result}, is ${result == minCost(*costs)}")
 }
