1145379c287d3730c2af41d0a7415f2456edcd4e
qiaoyuang <qiaoyuang2012@gmail.com>
2024-01-18 12:08:06 +0800

    Perfect the Questions 91

--- src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions91.kt
+++ src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions91.kt
@@ -3,27 +3,25 @@ package com.qiaoyuang.algorithm.special
 import kotlin.math.min
 
 fun test91() {
-    println(
-        minCost(
-            arrayOf(
-                intArrayOf(17, 2, 16),
-                intArrayOf(15, 14, 5),
-                intArrayOf(13, 3, 1),
-            )
-        )
-    )
+    printlnResult(arrayOf(
+        intArrayOf(17, 2, 16),
+        intArrayOf(15, 14, 5),
+        intArrayOf(13, 3, 1),
+    ))
 }
 
 /**
  * Questions 91: Paint houses to red, green or blue. The houses that nearby can't be painted with same color.
  * Given a series IntArrays to represent the cost of painting, find the minimum cost.
  */
-private fun minCost(costs: Array<IntArray>): Int =
-    min(
+private fun minCost(costs: Array<IntArray>): Int {
+    require(costs.isNotEmpty()) { "The costs can't be empty" }
+    return min(
         costs red costs.lastIndex,
         costs green costs.lastIndex,
         costs blue costs.lastIndex,
-        )
+    )
+}
 
 private infix fun Array<IntArray>.red(i: Int): Int =
     if (i == 0)
@@ -45,3 +43,25 @@ private infix fun Array<IntArray>.blue(i: Int): Int =
 
 private fun min(a: Int, b: Int, c: Int): Int =
     min(min(a, b), c)
+
+private fun minCostInLoop(costs: Array<IntArray>): Int {
+    require(costs.isNotEmpty()) { "The costs can't be empty" }
+    val first = costs.first()
+    val cache = intArrayOf(first[0], first[1], first[2])
+    if (costs.size == 1)
+        return cache.min()
+    for (i in 1 ..< costs.size) {
+        val red = min(cache[1], cache[2]) + costs[i][0]
+        val green = min(cache[0], cache[2]) + costs[i][1]
+        val blue = min(cache[0], cache[1]) + costs[i][2]
+        cache[0] = red
+        cache[1] = green
+        cache[2] = blue
+    }
+    return cache.min()
+}
+
+private fun printlnResult(costs: Array<IntArray>) {
+    val result = minCostInLoop(costs)
+    println("The minimum cost is $result, is: ${result == minCost(costs)}")
+}
