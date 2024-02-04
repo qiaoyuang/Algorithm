66d94b4a8fa307020d515d4d5d54a77c0bab6c4f
qiaoyuang <qiaoyuang2012@gmail.com>
2024-02-04 18:54:27 +0800

    Add the Questions 93 file

--- src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions92.kt
+++ src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions92.kt
@@ -7,7 +7,7 @@ fun test92() {
 
 /**
  * Questions 92: A String just contain 0 and 1, 0 could be changed to 1, 1 could be changed to 0,
- * how many changes at least that we can lead all 0s in front of all 1s.
+ * how many times of changing at least that we can lead all 0s in front of all 1s.
  */
 private fun String.minFlipTimes(): Int {
     require(isNotEmpty()) { "The string must not be empty" }
---
+++ src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions93.kt
@@ -0,0 +1,14 @@
+package com.qiaoyuang.algorithm.special
+
+fun test93() {
+    println(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8))
+}
+
+/**
+ * Questions 93: The longest Fibonacci sequence
+ */
+private fun IntArray.longestFibonacci(): Int {
+    if (size < 3)
+        return 0
+
+}
