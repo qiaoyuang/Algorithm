f179fde72afd454a5fb557eea8104b61344e6ff5
qiaoyuang <qiaoyuang2012@gmail.com>
2024-02-15 13:40:40 +0800

    Completed Questions 97

--- src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions97.kt
+++ src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions97.kt
@@ -11,6 +11,28 @@ fun test97() {
  */
 private fun countOfSubsequences(s: String, t: String): Int {
     require(s.isNotEmpty() && t.isNotEmpty()) { "The parameters can't be empty" }
+    val db = Array(s.length) { IntArray(t.length) }
+    s.forEachIndexed { i, cs ->
+        for (j in 0..min(i, t.lastIndex))
+            db[i][j] = if (t[j] == cs) when {
+                i - 1 < 0 && j - 1 < 0 -> 1
+                i - 1 >= 0 && j - 1 < 0 -> db[i - 1][j] + 1
+                else -> db[i - 1][j - 1] + db[i - 1][j]
+            } else {
+                if (i - 1 < 0) 0 else db[i - 1][j]
+            }
+    }
+    db.forEach {
+        it.forEach { i ->
+            print("$i ")
+        }
+        println()
+    }
+    return db.last().last()
+}
+
+private fun countOfSubsequences2(s: String, t: String): Int {
+    require(s.isNotEmpty() && t.isNotEmpty()) { "The parameters can't be empty" }
     val db = IntArray(t.length + 1)
     db[0] = 1
     s.forEachIndexed { i, cs ->
@@ -22,4 +44,4 @@ private fun countOfSubsequences(s: String, t: String): Int {
 }
 
 private fun printlnResult(s: String, t: String) =
-    println("The count of all sub-sequences in $s that equal $t is ${countOfSubsequences(s, t)}")
+    println("The count of all sub-sequences in $s that equal $t is ${countOfSubsequences(s, t)}, ${countOfSubsequences2(s, t)}")
