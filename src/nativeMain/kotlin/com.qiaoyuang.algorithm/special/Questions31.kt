25b1753a33846475d86b4065cb31d9fda9bdabfb
qiaoyuang <qiaoyuang2012@gmail.com>
2024-04-08 13:43:13 +0800

    Fix the Questions 31

--- src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions31.kt
+++ src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions31.kt
@@ -1,39 +1,72 @@
 package com.qiaoyuang.algorithm.special
 
 fun test31() {
-    printlnResult(1 to "abc", 2 to "def", 3 to "ghi", 4 to "jkl", 5 to "mno")
+    printlnResult(1 to 1, 2 to 2, 3 to 3, 4 to 4, 5 to 5)
 }
 
 /**
  * Questions 31: LRU cache
  */
-private class LRUCache<K, V>(private val maxSize: Int) {
+private class LRUCache(private val capacity: Int) {
 
-    private val map = HashMap<K, V>(maxSize)
-    private val keyList = ArrayDeque<K>()
+    private val firstNode = ListNode(-1, -1)
+    private val lastNode = ListNode(-1, -1)
 
-    operator fun get(key: K): V? {
-        if (!map.contains(key))
-            return null
-        keyList.remove(key)
-        keyList.addFirst(key)
-        return map[key]
+    private val map = HashMap<Int, ListNode>()
+
+    init {
+        firstNode.right = lastNode
+        lastNode.left = firstNode
     }
 
-    operator fun set(key: K, value: V) {
-        if (map.contains(key))
-            keyList.remove(key)
-        keyList.addFirst(key)
-        map[key] = value
-        if (keyList.size > maxSize)
-            map.remove(keyList.removeLast())
+    fun get(key: Int): Int {
+        val node = map[key] ?: return -1
+        node.left?.right = node.right
+        node.right?.left = node.left
+        node.left = firstNode
+        node.right = firstNode.right
+        firstNode.right = node
+        return node.value
     }
+
+    operator fun set(key: Int, value: Int) {
+        val newNode = when {
+            map.containsKey(key) -> {
+                val node = map[key]!!
+                node.left?.right = node.right
+                node.right?.left = node.left
+                node.value = value
+                node
+            }
+            map.size < capacity -> ListNode(key, value)
+            else -> {
+                val node = lastNode.left!!
+                map.remove(node.key)
+                node.left?.right = lastNode
+                lastNode.left = node.left
+                node.value = value
+                node
+            }
+        }
+        newNode.left = firstNode
+        newNode.right = firstNode.right
+        firstNode.right?.left = newNode
+        firstNode.right = newNode
+        map[key] = newNode
+    }
+
+    private class ListNode(
+        var key: Int,
+        var value: Int,
+        var left: ListNode? = null,
+        var right: ListNode? = null,
+    )
 
     override fun toString(): String = map.toString()
 }
 
-private fun printlnResult(vararg pairs: Pair<Int, String>) {
-    val cache = LRUCache<Int, String>(3)
+private fun printlnResult(vararg pairs: Pair<Int, Int>) {
+    val cache = LRUCache(3)
     println("Put ${pairs.toList()}")
     pairs.forEach { (key, value) ->
         cache[key] = value
