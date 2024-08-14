7be46fad2e4ad84b02f90fcf32deef632726d42e
qiaoyuang <qiaoyuang2012@gmail.com>
2024-08-14 23:28:07 +0800

    Fix the questions 33

--- src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions33.kt
+++ src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions33.kt
@@ -7,40 +7,18 @@ fun test33() {
 /**
  * Questions 33: Give a string array, group strings as anagram
  */
-private fun Array<String>.groupAsAnagram(): List<List<String>> {
-    require(isNotEmpty()) { "The array can't be empty" }
-    val result = ArrayList<MutableList<String>>()
-    val hashArray = IntArray(size)
-    var realSize = 0
-    forEach {
-        require(it.isNotEmpty()) { "The string can't be empty" }
-        val hash = it.hash()
-        if (result.isEmpty()) {
-            hashArray[realSize++] = hash
-            result.add(mutableListOf(it))
-        } else {
-            var arrayIndex = 0
-            var isFound = false
-            while (hashArray[arrayIndex] > 0) {
-                if (hashArray[arrayIndex] == hash) {
-                    result[arrayIndex].add(it)
-                    isFound = true
-                    break
-                } else
-                    arrayIndex++
-            }
-            if (!isFound) {
-                hashArray[realSize++] = hash
-                result.add(mutableListOf(it))
-            }
-        }
+private fun groupAnagrams(strings: Array<String>): List<List<String>> {
+    val map = HashMap<String, MutableList<String>>()
+    strings.forEach {
+        val array = it.toCharArray()
+        array.sort()
+        val key = array.concatToString()
+        (map[key] ?: ArrayList<String>().also { list ->
+            map[key] = list
+        }).add(it)
     }
-    return result
+    return map.values.toList()
 }
 
-private val primeNumber = intArrayOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101)
-
-private fun String.hash(): Int = fold(initial = 1) { acc, c -> acc * primeNumber[c.code - 'a'.code] }
-
 private fun printlnResult(strings: Array<String>) =
-    println("Group ${strings.toList()}, we got: ${strings.groupAsAnagram()}")
+    println("Group ${strings.toList()}, we got: ${groupAnagrams(strings)}")
