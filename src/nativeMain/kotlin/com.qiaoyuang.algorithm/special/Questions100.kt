7283a8d0cbfdb0d230c13a6bdf2b1fb1632654c0
qiaoyuang <qiaoyuang2012@gmail.com>
2026-01-18 21:14:56 +0000

    Fix LeetCode number for Questions 100

--- src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions100.kt
+++ src/nativeMain/kotlin/com.qiaoyuang.algorithm/special/Questions100.kt
@@ -12,7 +12,7 @@ fun test100() {
 }
 
 /**
- * Questions 100: Triangle, LeetCode 100
+ * Questions 100: Triangle, LeetCode 120
  */
 private fun smallestPath(triangle: Array<out IntArray>): Int {
     require(triangle.isNotEmpty()) { "The triangle can't be empty" }
